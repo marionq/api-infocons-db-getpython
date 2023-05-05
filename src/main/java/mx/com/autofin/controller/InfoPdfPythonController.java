9bc25541af68d6b499cca071b1cca96a86b0bec2
mnepomuceno <mario.n.q@hotmail.com>
2023-05-05 17:04:18 -0600

    Update 20230505_1704

--- src/main/java/mx/com/autofin/controller/InfoPdfPythonController.java
+++ src/main/java/mx/com/autofin/controller/InfoPdfPythonController.java
@@ -83,7 +83,7 @@ public class InfoPdfPythonController {
                 return ResponseHandler.generateResponse("OK", HttpStatus.OK, infoConstancia);
             }
         } catch (Exception e) {
-            return ResponseHandler.generateResponse("Error", HttpStatus.INTERNAL_SERVER_ERROR, "Error message: " + e.getMessage());
+            return ResponseHandler.generateResponse("Formato no compatible", HttpStatus.NOT_ACCEPTABLE, "Error message: " + e.getMessage());
         }
 
     }
